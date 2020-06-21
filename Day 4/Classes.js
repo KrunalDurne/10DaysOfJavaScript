1a10d7a0c9030c60738a8b9f032dc6dd075e8941
KrunalDurne <48874464+KrunalDurne@users.noreply.github.com>
2020-06-21 09:31:24 +0530

    Classes.js
    Because Previous change couldn't be given the right output

--- Day 4/Classes.js
+++ Day 4/Classes.js
@@ -1,17 +1,15 @@
-'use strict'
+//Implement a Polygon class
 class Polygon{
-    constructor(sides){
-        this.sides = sides;
-        this.perimeter = this.perimeter;
-    }
-    perimeter() {
-        let sum = 0;
-        for(let i =0;i<this.sides.length;i++) {
-            sum = sum + this.sides[i];
-        }
-        return sum;
-    }
-}
 
-const triangle = new Polygon([3,4,5]);
-console.log(triangle.perimeter());
+  // Constructor that take an array of integer side lengths  
+    constructor(array){
+      this.array =  array;
+    }
+       perimeter(){
+           let res = 0;
+           for(let i = 0; i < this.array.length; i++){
+               res += this.array[i];
+
+           } return res;
+       }
+    }
